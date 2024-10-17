1c5d903a37b35792856c6d1e516fc1adf4cbafb2
phamduyphuong251 <phamduyphuong251@gmail.com>
2024-10-17 16:27:12 +0700

    fix sso

--- app/static/js/index.js
+++ app/static/js/index.js
@@ -271,7 +271,7 @@ function checkCookiesAndRedirect() {
     if (!user_id) {
         console.log("Cookies đã hết hạn, redirecting...");
         // Redirect về trang signin nếu cookies hết hạn
-        // window.location.href = '/signin';
+        window.location.href = '/signin';
     } else {
         console.log("Cookies vẫn hợp lệ");
     }
