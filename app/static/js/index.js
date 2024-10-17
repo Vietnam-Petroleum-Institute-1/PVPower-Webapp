0fb036caae2ddf39673f100bbe7989f185f0a3f2
phamduyphuong251 <phamduyphuong251@gmail.com>
2024-10-17 16:11:35 +0700

    fix sso

--- app/static/js/index.js
+++ app/static/js/index.js
@@ -7,6 +7,7 @@ function truncateText(text, maxLength) {
 }
 
 function getCookie(name) {
+    console.log(document.cookie);
     const value = `; ${document.cookie}`;
     const parts = value.split(`; ${name}=`);
     if (parts.length === 2) return parts.pop().split(';').shift();
