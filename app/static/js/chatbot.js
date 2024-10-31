5fd54bbd19141a8ac03ee6f4561eac683684b08e
phamduyphuong251 <phamduyphuong251@gmail.com>
2024-10-31 13:44:15 +0700

    update database

--- app/static/js/chatbot.js
+++ app/static/js/chatbot.js
@@ -452,7 +452,8 @@ function addMessageToChat(sender, message, messageId) {
 
   const messageContent = document.createElement("div");
   messageContent.classList.add("message-content");
-  messageContent.textContent = message;
+
+  messageContent.innerHTML = sender === "bot" ? marked(message) : message;
 
   messageElement.appendChild(messageContent);
 
